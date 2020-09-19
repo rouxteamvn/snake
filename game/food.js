cef2fc0795249d11027392933c863716bbb8b1f3
game-library <42694704+game-library@users.noreply.github.com>
2020-09-19 22:52:43 +0700

    mistake

--- game/food.js
+++ game/food.js
@@ -1,1 +1,77 @@
-'use strict';class Food{constructor(a=new Game){this.game=a,this.food={},this.theBiggerOne=null,this.randomScore=null,this.numFoodAte=0,this.foodImg=new Image,this.foodImg.src="img/food.webp"}init(){this.createFood()}createFood(){0!==this.numFoodAte&&0==this.numFoodAte%5?(this.food={x:Math.floor(16*Math.random()+1)*this.game.unit,y:Math.floor(14*Math.random()+3)*this.game.unit,w:2*this.game.unit,h:2*this.game.unit},this.theBiggerOne=!0,this.numFoodAte=0):(this.food={x:Math.floor(17*Math.random()+1)*this.game.unit,y:Math.floor(15*Math.random()+3)*this.game.unit,w:this.game.unit,h:this.game.unit},this.theBiggerOne=!1,this.numFoodAte++),this.checkRandomFood(this.game.snake.snakeBody)&&this.createFood()}checkRandomFood(a){for(let b=0,c=a.length;b<c;b++)if(this.foodCollision(a[b]))return!0;return!1}foodCollision(a){return a.x===this.food.x&&a.y===this.food.y||a.x===this.food.x&&a.y+this.game.unit===this.food.y+this.food.h||a.x+this.game.unit===this.food.x+this.food.w&&a.y===this.food.y||a.x+this.game.unit===this.food.x+this.food.w&&a.y+this.game.unit===this.food.y+this.food.h}draw(){const a=this.game.offContext;a.drawImage(this.foodImg,this.food.x,this.food.y,this.food.w,this.food.h)}}
+'use strict';
+
+class Food {
+    constructor(game = new Game()) {
+        this.game = game;
+        this.food = {};
+        this.theBiggerOne = null;
+        this.randomScore = null;
+        this.numFoodAte = 0;
+        this.foodImg = new Image();
+        this.foodImg.src = "img/food.webp";
+    }
+
+    init() {
+        this.createFood();
+    }
+
+    createFood() {
+        if (this.numFoodAte !== 0 &&
+            this.numFoodAte % 5 === 0) {
+            this.food = {
+                x: Math.floor(Math.random() * 16 + 1) * this.game.unit,
+                y: Math.floor(Math.random() * 14 + 3) * this.game.unit,
+                w: 2 * this.game.unit,
+                h: 2 * this.game.unit
+            };
+            this.theBiggerOne = true;
+            this.numFoodAte = 0;
+        } else {
+            this.food = {
+                x: Math.floor(Math.random() * 17 + 1) * this.game.unit,
+                y: Math.floor(Math.random() * 15 + 3) * this.game.unit,
+                w: this.game.unit,
+                h: this.game.unit
+            };
+            this.theBiggerOne = false;
+            this.numFoodAte++;
+        }
+
+        if (this.checkRandomFood(this.game.snake.snakeBody)) {
+            this.createFood();
+        }
+    }
+
+    checkRandomFood(head) {
+        for (let i = 0, length = head.length; i < length; i++) {
+            if (this.foodCollision(head[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    foodCollision(head) {
+        return (
+            head.x === this.food.x && head.y === this.food.y ||
+            head.x === this.food.x &&
+            head.y + this.game.unit === this.food.y + this.food.h ||
+            head.x + this.game.unit === this.food.x + this.food.w &&
+            head.y === this.food.y ||
+            head.x + this.game.unit === this.food.x + this.food.w &&
+            head.y + this.game.unit === this.food.y + this.food.h
+        );
+    }
+
+    draw() {
+        const offContext = this.game.offContext;
+        //draw food
+        offContext.drawImage(
+            this.foodImg,
+            this.food.x,
+            this.food.y,
+            this.food.w,
+            this.food.h
+        );
+    }
+}
--- game/game.js
+++ game/game.js
@@ -1,1 +1,255 @@
-"use strict";class Game{constructor(){this.offCanvas=document.getElementById("offCanvas"),this.offContext=this.offCanvas.getContext("2d"),this.width=this.offCanvas.width,this.height=this.offCanvas.height,this.div=document.getElementById("div-canvas"),this.canvas=document.createElement("canvas"),this.canvas.width=608,this.canvas.height=608,this.div.appendChild(this.canvas),this.context=this.canvas.getContext("2d"),this.unit=32,this.trackingFps={elapsed:0,then:0,now:0,fpsInterval:0},this.mode=null,this.requestId=null,this.menu=new Menu(this),this.snake=new Snake(this),this.food=new Food(this),this.prize=new Prize(this),this.groundImg=new Image,this.groundImg.src="img/ground.webp",this.lose=!1,this.cup=new Image,this.cup.src="img/cup.webp",this.restartImg=new Image,this.restartImg.src="img/restart.webp",this.restartBtn={x:7.3*this.unit,y:14*this.unit,w:140,h:54},this.themeAu=new Audio("audio/theme.mp3"),this.hasPlayed=!1}init(){this.snake.init(),this.food.init()}startAnimating(t){this.trackingFps.fpsInterval=1e3/t,this.trackingFps.now=Date.now(),this.play()}play(){this.requestId=requestAnimationFrame(this.play.bind(this)),this.trackingFps.now=Date.now(),this.trackingFps.elapsed=this.trackingFps.now-this.trackingFps.then,this.trackingFps.elapsed>this.trackingFps.fpsInterval&&(this.trackingFps.then=this.trackingFps.now-this.trackingFps.elapsed%this.trackingFps.fpsInterval,this.themeAu.volume=.1,this.themeAu.play(),this.update(),this.draw(),this.checkLose(this.requestId))}checkLose(t){this.lose&&(this.themeAu.pause(),cancelAnimationFrame(t))}update(){this.hasPlayed&&(this.snake.update(),this.prize.update())}formatString(t){if(0===t)return"000";const i=t.toString(),e=Math.floor(Math.log10(t));return 0===e?`${"0".repeat(2)}${i}`:1===e?`${"0".repeat(1)}${i}`:`${"0".repeat(0)}${i}`}draw(){this.offContext.drawImage(this.groundImg,0,0),this.hasPlayed?this.lose?(this.offContext.drawImage(this.cup,7*this.unit,7*this.unit,5*this.unit,5*this.unit),this.offContext.font="45px Changa One",this.offContext.fillStyle="rgb(0,0,0)",this.offContext.fillText(this.formatString(this.snake.score),8.1*this.unit,10.5*this.unit),this.offContext.font="32px Changa One",this.offContext.fillText(`High Score: ${this.snake.highScore.toString()}`,11*this.unit,1.5*this.unit),this.offContext.fillText("Ăn nhiều thì phải khát nước",3.9*this.unit,6.3*this.unit),this.offContext.fillText("Làm tách trà bình tĩnh lại bạn ei! ",3*this.unit,13*this.unit),this.offContext.drawImage(this.restartImg,this.restartBtn.x,this.restartBtn.y,this.restartBtn.w,this.restartBtn.h)):(this.food.draw(),this.prize.draw(),this.snake.draw()):this.menu.draw(),this.offContext.font="20px Changa One ",this.offContext.fillStyle="rgb(255,255,255)",this.offContext.fillText("Nếu có gặp lỗi đừng báo cho dev, nó cọc nó tới nhà nó đánh!",2*this.unit,18.7*this.unit),this.context.drawImage(this.offCanvas,0,0)}getMousePos(t,i){const e=t.getBoundingClientRect();return{x:i.clientX-e.left,y:i.clientY-e.top}}btnClick(t,i){const e=this.getMousePos(this.canvas,i);return e.x>=t.x&&e.x<=t.x+t.w&&e.y>=t.y&&e.y<=t.y+t.h}}window.onload=(()=>{let t=new Game;t.init(),t.startAnimating(10),document.addEventListener("click",i=>{t.hasPlayed?t.lose&&t.btnClick(t.restartBtn,i)&&(t.div.removeChild(t.canvas),(t=new Game).init(),t.startAnimating(10)):t.btnClick(t.menu.startBtn,i)?t.hasPlayed=!0:t.btnClick(t.menu.settingBtn,i)&&(t.menu.settingBtn.w=0,setTimeout(()=>{t.menu.settingBtn.w=2*t.unit},300))}),document.addEventListener("keydown",i=>{" "===i.key&&t.lose&&(t.div.removeChild(t.canvas),(t=new Game).init(),t.startAnimating(10));const e=t.snake.move;switch(i.key){case"ArrowLeft":"right"!==e&&(t.snake.move="left");break;case"ArrowUp":"down"!==e&&(t.snake.move="up");break;case"ArrowRight":"left"!==e&&(t.snake.move="right");break;case"ArrowDown":"up"!==e&&(t.snake.move="down");break;default:t.snake.move="stop"}})});
+'use strict';
+
+class Game {
+    constructor() {
+        //canvas stuff 
+        this.offCanvas = document.getElementById("offCanvas");
+        this.offContext = this.offCanvas.getContext("2d");
+        this.width = this.offCanvas.width;
+        this.height = this.offCanvas.height;
+
+        //wrap on-cavas to div element
+        this.div = document.getElementById("div-canvas");
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 608;
+        this.canvas.height = 608;
+        this.div.appendChild(this.canvas);
+        this.context = this.canvas.getContext("2d");
+        this.unit = 32;
+        this.trackingFps = {
+            elapsed: 0,
+            then: 0,
+            now: 0,
+            fpsInterval: 0
+        }
+
+        this.mode = null;
+        this.requestId = null;
+
+        this.menu = new Menu(this);
+        this.snake = new Snake(this);
+        this.food = new Food(this);
+        this.prize = new Prize(this);
+
+        this.groundImg = new Image();
+        this.groundImg.src = "img/ground.webp";
+        this.lose = false;
+
+        this.cup = new Image();
+        this.cup.src = "img/cup.webp";
+        this.restartImg = new Image();
+        this.restartImg.src = "img/restart.webp";
+        this.restartBtn = {
+            x: 7.3 * this.unit,
+            y: 14 * this.unit,
+            w: 140,
+            h: 54
+        }
+        this.themeAu = new Audio("audio/theme.mp3");
+        this.hasPlayed = false;
+    }
+
+    init() {
+        this.snake.init();
+        this.food.init();
+    }
+
+    startAnimating(fps) {
+        this.trackingFps.fpsInterval = 1000 / fps;
+        this.trackingFps.now = Date.now();
+
+        this.play();
+    }
+
+    play() {
+        this.requestId = requestAnimationFrame(this.play.bind(this));
+
+        this.trackingFps.now = Date.now();
+        this.trackingFps.elapsed = this.trackingFps.now - this.trackingFps.then;
+        if (this.trackingFps.elapsed > this.trackingFps.fpsInterval) {
+            this.trackingFps.then = this.trackingFps.now - (this.trackingFps.elapsed % this.trackingFps.fpsInterval);
+            this.themeAu.volume = 0.1;
+            this.themeAu.play();
+            this.update();
+            this.draw();
+            this.checkLose(this.requestId);
+        }
+    }
+
+    checkLose(requestId) {
+        if (this.lose) {
+            this.themeAu.pause();
+            cancelAnimationFrame(requestId);
+        }
+    }
+    update() {
+        if (this.hasPlayed) {
+            this.snake.update();
+            this.prize.update();
+        }
+    }
+
+    formatString(score) {
+        if (score === 0) {
+            return `000`;
+        }
+
+        const scoreStr = score.toString();
+        const numOfDigits = Math.floor(Math.log10(score));
+        return numOfDigits === 0 ? `${"0".repeat(2)}${scoreStr}` :
+            numOfDigits === 1 ? `${"0".repeat(1)}${scoreStr}` :
+            `${"0".repeat(0)}${scoreStr}`;
+    }
+
+    draw() {
+        // draw ground
+        this.offContext.drawImage(this.groundImg, 0, 0);
+
+        if (!this.hasPlayed) {
+            this.menu.draw();
+        } else {
+            if (!this.lose) {
+                this.food.draw();
+                this.prize.draw();
+                this.snake.draw();
+            } else {
+                this.offContext.drawImage(
+                    this.cup,
+                    7 * this.unit,
+                    7 * this.unit,
+                    5 * this.unit,
+                    5 * this.unit
+                );
+                this.offContext.font = "45px Changa One";
+                this.offContext.fillStyle = "rgb(0,0,0)";
+
+                // draw game over
+                this.offContext.fillText(
+                    this.formatString(this.snake.score),
+                    8.1 * this.unit,
+                    10.5 * this.unit
+                );
+
+                this.offContext.font = "32px Changa One";
+                this.offContext.fillText(
+                    `High Score: ${this.snake.highScore.toString()}`,
+                    11 * this.unit,
+                    1.5 * this.unit
+                )
+
+                this.offContext.fillText(
+                    "Ăn nhiều thì phải khát nước",
+                    3.9 * this.unit,
+                    6.3 * this.unit
+                );
+                this.offContext.fillText(
+                    "Làm tách trà bình tĩnh lại bạn ei! ",
+                    3 * this.unit,
+                    13 * this.unit
+                );
+
+                this.offContext.drawImage(
+                    this.restartImg,
+                    this.restartBtn.x,
+                    this.restartBtn.y,
+                    this.restartBtn.w,
+                    this.restartBtn.h
+                );
+
+
+
+            }
+        }
+
+        this.offContext.font = "20px Changa One ";
+        this.offContext.fillStyle = "rgb(255,255,255)";
+        this.offContext.fillText("Nếu có gặp lỗi đừng báo cho dev, nó cọc nó tới nhà nó đánh!", 2 * this.unit, 18.7 * this.unit);
+        this.context.drawImage(this.offCanvas, 0, 0);
+    }
+
+
+    getMousePos(canvas, e) {
+        const rect = canvas.getBoundingClientRect();
+        return {
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top
+        }
+    }
+
+    btnClick(btn, e) {
+        const mousePos = this.getMousePos(this.canvas, e);
+        return (
+            mousePos.x >= btn.x &&
+            mousePos.x <= btn.x + btn.w &&
+            mousePos.y >= btn.y &&
+            mousePos.y <= btn.y + btn.h);
+    }
+}
+
+window.onload = () => {
+    const fps = 10;
+    let game = new Game();
+    game.init();
+    game.startAnimating(fps);
+
+    document.addEventListener("click", (e) => {
+        if (!game.hasPlayed) {
+            if (game.btnClick(game.menu.startBtn, e)) {
+                game.hasPlayed = true;
+            } else if (game.btnClick(game.menu.settingBtn, e)) {
+                game.menu.settingBtn.w = 0;
+                setTimeout(() => {
+                    game.menu.settingBtn.w = 2 * game.unit;
+                }, 300);
+            }
+        } else {
+            if (game.lose) {
+                // click restart button
+                if (game.btnClick(game.restartBtn, e)) {
+                    game.div.removeChild(game.canvas);
+                    game = new Game();
+                    game.init();
+                    game.startAnimating(fps);
+                }
+            }
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === " ") {
+            if (game.lose) {
+                game.div.removeChild(game.canvas);
+                game = new Game();
+                game.init();
+                game.startAnimating(fps);
+            }
+        }
+
+        const move = game.snake.move;
+        switch (e.key) {
+            case "ArrowLeft":
+                if (move !== "right") {
+                    game.snake.move = "left";
+                }
+                break;
+            case "ArrowUp":
+                if (move !== "down") {
+                    game.snake.move = "up";
+                }
+                break;
+            case "ArrowRight":
+                if (move !== "left") {
+                    game.snake.move = "right";
+                }
+                break;
+            case "ArrowDown":
+                if (move !== "up") {
+                    game.snake.move = "down";
+                }
+                break;
+            default:
+                game.snake.move = "stop";
+                break;
+        }
+    });
+}
